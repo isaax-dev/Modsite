09bdef88763627e3757b1b194cb8e74a7b9e5c23
isaax-dev <zippnew60@gmail.com>
2025-12-14 00:30:44 -0300

    Change home route response message

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+# cria a aplicação
+app = Flask(__name__)
+
+# rota principal
 @app.route("/")
 def home():
-    return "Servidor online 🔥"
+    return "Modsite online 🔥"
+
+# roda o servidor
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
